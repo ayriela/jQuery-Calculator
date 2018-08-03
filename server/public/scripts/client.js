2206317fe73a125e4ef1209c3de238c33547b80e
Christopher Black <chris@primeacademy.io>
2018-08-03 14:16:31 -0500

    Update client.js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,11 +1,3 @@
-class Calculation{
-  constructor( xIn, yIn, operatorIn ){
-    this.x = xIn;
-    this.y = yIn;
-    this.operator = operatorIn;
-  } //end constructor
-} // end class
-
 $( document ).ready( readyNow );
 
 function readyNow(){
